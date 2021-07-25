5b05a77757821d86f022964b2243ffcd64ddbb3b
ErgoSphere <melanriscn@hotmail.com>
2021-07-25 21:28:57 +0800

    touch

---
+++ src/views/Examples/触摸手势判断.vue
@@ -0,0 +1,37 @@
+<template>
+<div @touchstart="startEvent" @touchmove="moveEvent">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "竖向触摸手势判断",
+
+  data () {
+    return {
+      startY: 0
+    }
+  },
+
+  methods: {
+    startEvent: function(e) {
+      this.startY = e.changedTouches[0].pageY
+    },
+
+    moveEvent: function(e) {
+      let moveEndY = e.changedTouches[0].pageY
+      let diff = moveEndY - this.startY
+      if (diff > 0) {
+        console.log('下滑')
+      } else if (diff < 0) {
+        console.log('上滑')
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
